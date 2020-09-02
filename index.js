1393a21400f5659530b4a26de03446024874cef3
Kyle Hansen <1hansen.kyle@gmail.com>
2020-09-02 15:42:10 -0600

    node 3 afternoon

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ massive({
     app.listen(SERVER_PORT, () => console.log(`I am alive ${SERVER_PORT}`))
 })
 .catch(err => console.log(err))
-
-
 app.post('/api/products', products_controller.create)
 app.get('/api/products', products_controller.getAll)
 app.get(`/api/products/:id`, products_controller.getOne)
 app.put('/api/products/:id', products_controller.update)
 app.delete('/api/products/:id', products_controller.delete)
+
+
 
 
 
